12a2c7762067bfcb72af9416438e88cef44053ef
purple-jwl <purple.jwl@gmail.com>
2019-04-13 17:39:02 +0900

    add ABC110 (D)

---
+++ src/ABC/110/D.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP (i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long ll;
+
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+const int MOD = 1000000007;
+
+ll modFact[111111];
+ll modInv[111111];
+ll modFactInv[111111];
+
+// calc nCr % mod
+ll comb(int n, int r) {
+  return modFact[n] * (modFactInv[r] * modFactInv[n - r] % MOD) % MOD;
+}
+
+int main() {
+  modFact[0] = modFact[1] = 1;
+  modFactInv[0] = modFactInv[1] = 1;
+  modInv[1] = 1;
+  REP (i, 2, 111111) {
+    modFact[i] = modFact[i - 1] * i % MOD;
+    modInv[i] = MOD - modInv[MOD % i] * (MOD / i) % MOD;
+    modFactInv[i] = modFactInv[i - 1] * modInv[i] % MOD;
+  }
+
+  int N, M;
+  cin >> N >> M;
+
+  map<int, int> pCnts;
+  int m = M;
+  for (int i = 2; i * i <= M; i++) {
+    while (m % i == 0) {
+      m /= i;
+      pCnts[i]++;
+    }
+  }
+  if (m != 1) pCnts[m]++;
+
+  ll ans = 1;
+  for (auto pCnt : pCnts) {
+    ans = (ans * comb(N - 1 + pCnt.S, N - 1)) % MOD;
+  }
+
+  cout << ans << endl;
+}
