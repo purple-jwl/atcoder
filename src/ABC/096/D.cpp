018258c2b9338d786ea0eef40d2ab76891efd106
purple-jwl <purple.jwl@gmail.com>
2019-04-21 23:46:55 +0900

    add ABC096 (D)

---
+++ src/ABC/096/D.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP (i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long ll;
+
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  vector<int> primes;
+
+  REP (i, 2, 55556) {
+    if (i % 5 != 1) continue;
+
+    bool isPrime = true;
+    for (int j = 2; j * j <= i; j++) {
+      if (i % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) primes.eb(i);
+  }
+
+  int n;
+  cin >> n;
+
+  rep (i, n) cout << (i == 0 ? "" : " ") << primes[i];
+  cout << endl;
+}
