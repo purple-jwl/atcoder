62b4417617ec30b092be31f77c3c0783d0c0679e
purple-jwl <purple.jwl@gmail.com>
2019-04-27 15:02:07 +0900

    add ABC072 (D)

---
+++ src/ABC/072/D.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP (i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long ll;
+
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> p(n);
+  rep (i, n) {
+    cin >> p[i];
+    p[i]--;
+  }
+
+  int ans = 0;
+  rep (i, n) {
+    if (p[i] != i) continue;
+    ans++;
+    if (i + 1 < n) swap(p[i], p[i + 1]);
+  }
+
+  cout << ans << endl;
+}
