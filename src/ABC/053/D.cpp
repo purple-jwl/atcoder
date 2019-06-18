7d531a0c12013809a5859ba97a91992e189b7b2b
purple-jwl <purple.jwl@gmail.com>
2019-06-18 22:22:19 +0900

    add ABC053 (D)

---
+++ src/ABC/053/D.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP (i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long ll;
+
+// const int INF = 1 << 29;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int cnt[100000 + 1];
+  memset(cnt, 0, sizeof(cnt));
+
+  rep (i, N) {
+    int a;
+    cin >> a;
+    cnt[a]++;
+  }
+
+  int sum = 0;
+  rep (i, 100000 + 1) sum += max(0, cnt[i] - 1);
+
+  cout << N - sum - sum % 2 << endl;
+}
