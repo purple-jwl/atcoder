ac5d22af53d4ad2e845b9acd38c6ba7cb4854355
purple-jwl <purple.jwl@gmail.com>
2019-06-27 23:45:36 +0900

    add ABC120 (ABC)

---
+++ src/ABC/120/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  int a, b, k;
+  cin >> a >> b >> k;
+
+  int cnt = 0;
+  for (int i = 100;; i--) {
+    if (a % i == 0 && b % i == 0) cnt++;
+    if (cnt == k) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+}
