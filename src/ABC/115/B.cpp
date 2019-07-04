97cdf593a656a9009d418b4ecfc20e136978fa86
purple-jwl <purple.jwl@gmail.com>
2019-07-04 22:26:32 +0900

    add ABC115 (ABC)

---
+++ src/ABC/115/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> p(n);
+  rep(i, n) cin >> p[i];
+  sort(all(p));
+  int ans = 0;
+  rep(i, n) ans += (i != n - 1) ? p[i] : p[i] / 2;
+  cout << ans << endl;
+}
