df5809b27d6159f4fb1dff25f135f0a276a91dbf
purple-jwl <purple.jwl@gmail.com>
2019-07-15 21:36:30 +0900

    add ABS (IJK)

---
+++ src/ABC/049/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  string s;
+  cin >> s;
+  reverse(all(s));
+  string arr[] = {"dream", "dreamer", "erase", "eraser"};
+  rep(i, 4) reverse(all(arr[i]));
+
+  bool f1 = true;
+  rep(i, s.size()) {
+    bool f2 = false;
+    rep(j, 4) {
+      bool f3 = true;
+      rep(k, arr[j].size()) {
+        if (i + k >= s.size() || s[i + k] != arr[j][k]) {
+          f3 = false;
+          break;
+        }
+      }
+      if (f3) {
+        f2 = true;
+        i += arr[j].size() - 1;
+        break;
+      }
+    }
+    if (!f2) {
+      f1 = false;
+      break;
+    }
+  }
+
+  cout << (f1 ? "YES" : "NO") << endl;
+}
