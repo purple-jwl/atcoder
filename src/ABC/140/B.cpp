1d38f35eed38024408c883e74eb21c92f085eb4e
purple-jwl <purple.jwl@gmail.com>
2019-09-07 23:40:28 +0900

    add ABC140 (ABCD)

---
+++ src/ABC/140/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n), c(n - 1);
+  rep(i, n) cin >> a[i];
+  rep(i, n) cin >> b[i];
+  rep(i, n - 1) cin >> c[i];
+
+  int ans = 0;
+  rep(i, n) {
+    ans += b[a[i] - 1];
+    if (i != 0 && a[i - 1] + 1 == a[i]) ans += c[a[i - 1] - 1];
+  }
+
+  cout << ans << endl;
+}
