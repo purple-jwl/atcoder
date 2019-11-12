a215331897805d5174a53cbe555b1815072653ef
purple-jwl <purple.jwl@gmail.com>
2019-11-12 13:57:20 +0900

    add ABC067 (C)

---
+++ src/ABC/067/C.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> sum(n + 1);
+  rep(i, n) {
+    int a;
+    cin >> a;
+    sum[i + 1] = sum[i] + a;
+  }
+
+  ll ans = abs((sum[n] - sum[1]) - sum[1]);
+  REP(i, 1, n) ans = min(ans, abs((sum[n] - sum[i]) - sum[i]));
+
+  cout << ans << endl;
+}
