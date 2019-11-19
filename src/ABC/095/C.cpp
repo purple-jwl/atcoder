e87fbd6a68e8d0d4be3e92d0709bd1f8cb055a70
purple-jwl <purple.jwl@gmail.com>
2019-11-19 14:43:24 +0900

    add some files

---
+++ src/ABC/095/C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int a, b, c, x, y;
+  cin >> a >> b >> c >> x >> y;
+
+  int ans = a * x + b * y;
+  int tmp = ans;
+
+  rep(i, max(x, y)) {
+    if (i < x) tmp -= a;
+    if (i < y) tmp -= b;
+    tmp += 2 * c;
+    ans = min(ans, tmp);
+  }
+
+  cout << ans << endl;
+}
