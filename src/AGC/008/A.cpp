bdefb897a51e787fd7f1de4b19b71dc05639f3db
purple-jwl <purple.jwl@gmail.com>
2020-01-21 23:42:26 +0900

    add AGC008 (A)

---
+++ src/AGC/008/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+
+  if (x <= y) {
+    if ((x >= 0 && y >= 0) || (x <= 0 && y <= 0)) {
+      cout << (y - x) << endl;
+    } else {
+      cout << abs(abs(x) - y) + 1 << endl;
+    }
+  } else {
+    if ((x >= 0 && y >= 0) || (x <= 0 && y <= 0)) {
+      cout << x - y + ((x == 0 || y == 0) ? 1 : 2) << endl;
+    } else {
+      cout << abs(abs(y) - x) + 1 << endl;
+    }
+  }
+}
