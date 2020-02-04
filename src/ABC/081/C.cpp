94711c832302e0fff36b51b964e27b9dabb7b199
purple-jwl <purple.jwl@gmail.com>
2020-02-04 13:43:21 +0900

    add some files

---
+++ src/ABC/081/C.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> cnt(200000 + 1, 0);
+  rep(i, n) {
+    int a;
+    cin >> a;
+    cnt[a]++;
+  }
+
+  vector<int> vp;
+  rep(i, cnt.size()) if (cnt[i]) vp.eb(cnt[i]);
+
+  sort(rall(cnt));
+
+  int ans = 0;
+  REP(i, k, cnt.size()) ans += cnt[i];
+
+  cout << ans << endl;
+}
