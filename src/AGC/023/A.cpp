9a8161bcd1a1d6fb495bed1ea51e2103421531d7
purple-jwl <purple.jwl@gmail.com>
2020-02-10 23:53:49 +0900

    add some files

---
+++ src/AGC/023/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  vector<ll> sum(n, 0);
+  map<ll, int> cnt;
+  rep(i, n) {
+    if (i) sum[i] = sum[i - 1];
+    sum[i] += a[i];
+    cnt[sum[i]]++;
+  }
+
+  ll ans = 0;
+  rep(i, n) {
+    ll v = (a[i] - sum[i]) * -1;
+    ans += cnt[v];
+    cnt[sum[i]]--;
+  }
+
+  cout << ans << endl;
+}
