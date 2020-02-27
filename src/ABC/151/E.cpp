ea05f53df4b301f973c13ef2083d8ceb0a56e515
purple-jwl <purple.jwl@gmail.com>
2020-02-28 00:09:24 +0900

    add ABC151 (E)

---
+++ src/ABC/151/E.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+const ll MOD = 1000000007;
+
+const int MAX = 2000005;
+ll modFact[MAX];
+ll modInv[MAX];
+ll modFactInv[MAX];
+
+// calc nCr % mod
+ll comb(int n, int r) {
+  return modFact[n] * (modFactInv[r] * modFactInv[n - r] % MOD) % MOD;
+}
+
+void init() {
+  modFact[0] = modFact[1] = 1;
+  modFactInv[0] = modFactInv[1] = 1;
+  modInv[1] = 1;
+  REP(i, 2, MAX) {
+    modFact[i] = modFact[i - 1] * i % MOD;
+    modInv[i] = MOD - modInv[MOD % i] * (MOD / i) % MOD;
+    modFactInv[i] = modFactInv[i - 1] * modInv[i] % MOD;
+  }
+}
+
+int main() {
+  init();
+
+  int n, k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  sort(all(a));
+
+  ll ans = 0;
+  rep(i, n) {
+    int j = n - i - 1;
+    if (j < k - 1) break;
+    ans = (ans + MOD + a[n - 1 - i] * comb(j, k - 1) % MOD) % MOD;
+    ans = (ans + MOD - a[i] * comb(j, k - 1) % MOD) % MOD;
+  }
+
+  cout << ans << endl;
+}
