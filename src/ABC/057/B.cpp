ff298aa239135a8e45986ce4cf83eb8dabcdf0d3
purple-jwl <purple.jwl@gmail.com>
2020-03-03 14:13:35 +0900

    add some files

---
+++ src/ABC/057/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n), b(n), c(m), d(m);
+  rep(i, n) cin >> a[i] >> b[i];
+  rep(i, m) cin >> c[i] >> d[i];
+
+  rep(i, n) {
+    int dist = 1000000000;
+    int ans = -1;
+    rep(j, m) {
+      int _dist = abs(a[i] - c[j]) + abs(b[i] - d[j]);
+      if (dist > _dist) {
+        ans = j + 1;
+        dist = _dist;
+      }
+    }
+    cout << ans << endl;
+  }
+}
