a12ca9f8a63dfdb7beec681db87417b6956322fc
purple-jwl <purple.jwl@gmail.com>
2020-03-10 14:09:03 +0900

    add some files

---
+++ src/ABC/061/C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> cnt(100001, 0);
+  rep(i, n) {
+    int a, b;
+    cin >> a >> b;
+    cnt[a] += b;
+  }
+
+  rep(i, 100001) {
+    k -= cnt[i];
+    if (k <= 0) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+}
