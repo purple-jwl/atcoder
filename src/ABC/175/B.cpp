79ac52fe05ec49070df063070790226a61fe3d7b
purple-jwl <purple.jwl@gmail.com>
2020-08-18 13:46:34 +0900

    add some files

---
+++ src/ABC/175/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> l(n);
+  rep(i, n) cin >> l[i];
+  sort(all(l));
+
+  int ans = 0;
+  REP(i, 0, n) REP(j, i + 1, n) REP(k, j + 1, n) {
+    if (l[i] == l[j] || l[j] == l[k]) continue;
+    if (l[i] + l[j] > l[k]) ans++;
+  }
+
+  cout << ans << endl;
+}
