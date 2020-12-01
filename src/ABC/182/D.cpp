6b28abfa09bd36672029feadb6b5881151835b82
purple-jwl <purple.jwl@gmail.com>
2020-12-01 14:08:57 +0900

    add ABC182 (D)

---
+++ src/ABC/182/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<ll> a(n), sum(n), maxsum(n);
+  ll maxv = 0;
+  rep(i, n) cin >> a[i];
+
+  rep(i, n) {
+    sum[i] = a[i];
+    if (i) sum[i] += sum[i - 1];
+    maxv = max(maxv, sum[i]);
+    maxsum[i] = maxv;
+  }
+
+  ll ans = 0;
+  ll t = 0;
+  rep(i, n) {
+    ans = max(ans, t + maxsum[i]);
+    t += sum[i];
+  }
+
+  cout << ans << endl;
+}
