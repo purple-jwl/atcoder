7ce12646f729688fd01423bb934649378aff9082
purple-jwl <purple.jwl@gmail.com>
2021-01-18 10:53:15 +0900

    add ABC181 (E)

---
+++ src/ABC/181/E.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<ll> h(n), w(m);
+  rep(i, n) cin >> h[i];
+  rep(i, m) cin >> w[i];
+
+  sort(all(h));
+  sort(all(w));
+
+  vector<ll> sum(n + 1, 0);
+  rep(i, n - 1) { sum[i + 2] = sum[i] + h[i + 1] - h[i]; }
+
+  ll ans = INF;
+
+  for (int i = 0; i < n; i += 2) {
+    int idx = lower_bound(all(w), h[i]) - w.begin();
+    ll d = INF;
+    if (idx < m) d = min(d, w[idx] - h[i]);
+    if (0 < idx) d = min(d, h[i] - w[idx - 1]);
+    d += sum[i];
+    d += sum[n] - sum[i + 1];
+    ans = min(ans, d);
+  }
+
+  cout << ans << endl;
+}
