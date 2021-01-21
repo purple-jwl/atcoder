53c15154e65ac52a44da18c10f36e89e156980bf
purple-jwl <purple.jwl@gmail.com>
2021-01-21 14:05:58 +0900

    add ABC164 (D)

---
+++ src/ABC/164/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+const int MOD = 2019;
+
+int main() {
+  string s;
+  cin >> s;
+  reverse(all(s));
+
+  vector<int> cnt(MOD, 0);
+  cnt[0] = 1;
+  int ans = 0;
+  int v = 0;
+  int d = 1;
+  rep(i, s.size()) {
+    v = (v + d * (s[i] - '0')) % MOD;
+    d = d * 10 % MOD;
+    ans += cnt[v];
+    cnt[v]++;
+  }
+
+  // int ans = 0;
+  // rep(i, MOD) if (cnt[i] > 0) ans += cnt[i] * (cnt[i] - 1) / 2;
+
+  cout << ans << endl;
+}
