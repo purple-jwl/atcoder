ad458c669fabee16652fc33f6d8877452b7795cf
purple-jwl <purple.jwl@gmail.com>
2021-09-19 17:52:18 +0900

    add D

---
+++ src/ABC/217/D.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+typedef long long ll;
+
+/* --- INFやMODの値が適切か確認する --- */
+// const int INF = 1 << 30;
+// const ll INF = 1LL << 60;
+// const ll MOD = 1000000007;
+
+int main() {
+  int l, q;
+  cin >> l >> q;
+
+  set<int> st;
+  st.insert(0);
+  st.insert(l);
+
+  while (q--) {
+    int c, x;
+    cin >> c >> x;
+    if (c == 1) st.insert(x);
+    else {
+      auto it = st.lower_bound(x);
+      cout << *it - *(prev(it)) << endl;
+    }
+  }
+}
