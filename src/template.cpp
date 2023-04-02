e3b55763b8a19c770ee7c78a7193494ee691f105
purple-jwl <purple.jwl@gmail.com>
2023-04-02 13:43:59 +0900

    update

--- src/template.cpp
+++ src/template.cpp
@@ -5,20 +5,9 @@
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
-#define F first
-#define S second
-#define mp make_pair
-#define eb emplace_back
-#define pii pair<int, int>
-#define pll pair<ll, ll>
 
 using namespace std;
 
 typedef long long ll;
-
-/* --- INFやMODの値が適切か確認する --- */
-// const int INF = 1 << 30;
-// const ll INF = 1LL << 60;
-// const ll MOD = 1000000007;
 
 int main() {}
