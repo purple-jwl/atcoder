9a70913d8d2d67b3dbea7771fc855eacba7bdc74
purple-jwl <purple.jwl@gmail.com>
2023-04-11 11:39:10 +0900

    add

---
+++ src/ABC/266/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s[s.size() / 2] << endl;
+}
---
+++ src/ABC/266/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uniq(x) x.erase(unique(x.begin(), x.end()), x.end())
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  ll mx = 998244353;
+
+  ll n;
+  cin >> n;
+  while (n < 0) n += mx;
+  cout << n % mx << endl;
+}
